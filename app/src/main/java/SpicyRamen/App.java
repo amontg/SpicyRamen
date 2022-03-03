3f44b785c21f42351f7ff26bd8fb73c7029c5c39
Amir Montgomery <amirj.montg@gmail.com>
2022-03-03 13:31:26 -0800

    Logger
    Initial commit for logging config

--- app/src/main/java/SpicyRamen/App.java
+++ app/src/main/java/SpicyRamen/App.java
@@ -5,21 +5,29 @@
 */
 package SpicyRamen;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
 public class App 
 {
+    private static Logger logger = LogManager.getLogger(App.class);
     
     public static void main(String[] args) 
     {
         config config = new SpicyRamen.config();
         DiscordApi api = new DiscordApiBuilder().setToken(config.token).login().join();
         
-        // Print bot invite link
-        System.out.println("Invite me to a server: " + api.createBotInvite());
+            // Print bot invite link
+        //System.out.println("Invite me to a server: " + api.createBotInvite());
+        logger.info("Invite me to your server: " + api.createBotInvite());
 
-        // some message listeners
+            // some message listeners
         api.addMessageCreateListener(new PingPong());
+
+            // log bot server joins and leaves
+        api.addServerJoinListener(event -> logger.info("Joined server " + event.getServer().getName()));
+        api.addServerLeaveListener(event -> logger.info("Left server " + event.getServer().getName()));
     }
 }
